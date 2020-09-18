ac82c723547227a02e13699c048485e96472d636
jj0e3q <ndarklordfood@gmail.com>
2020-09-18 08:48:26 +0600

    к

--- assets/css/js/text.js
+++ assets/css/js/text.js
@@ -3,19 +3,6 @@ $(function() {
       animSeq = [""],
         animIndex = 1,
         titleIndex = 0;
-    function doInverseSpinZeroPitch() {
-        var loadTitle = title.substring(0, titleIndex);
-        if (titleIndex > title.length) {
-            animIndex = 0;
-            titleIndex = 0
-        }
-        if (animIndex > 1) {
-            titleIndex++;
-            animIndex = 0
-        }
-        document.title = loadTitle
-        animIndex++
-    }
     window.setInterval(doInverseSpinZeroPitch, 200);
 });
 $(function() {
